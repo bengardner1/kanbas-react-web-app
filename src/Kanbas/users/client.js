9e291c615302194bcf3a2f3ea44450e18bed47e8
bengardner1 <bengardner433@gmail.com>
2023-11-30 15:15:29 -0500

    changed mongo link

--- src/Kanbas/users/client.js
+++ src/Kanbas/users/client.js
@@ -3,8 +3,10 @@ const request = axios.create({
     withCredentials: true,
   });  
 //export const BASE_API = process.env.REACT_APP_BASE_API_URL;
-export const BASE_API = "https://bg-kanbas-node-server-app-d9dfae0193b7.herokuapp.com/";
-export const USERS_API = `${BASE_API}/api/users`;
+//export const BASE_API = "https://bg-kanbas-node-server-app-d9dfae0193b7.herokuapp.com/";
+//export const USERS_API = `${BASE_API}/api/users`;
+//export const USERS_API = "http://localhost:4000/api/users";
+export const USERS_API = "https://kanbas-node-server-app-fpid.onrender.com/api/users";
 export const signin = async (credentials) => {
   const response = await request.post( `${USERS_API}/signin`, credentials );
   return response.data;
